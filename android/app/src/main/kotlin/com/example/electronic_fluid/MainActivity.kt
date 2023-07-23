42320935853b5601192a82ba591fbd18521b344d
tkmuse <tkmuse@naver.com>
2023-07-23 22:59:28 +0530

    전자액자

---
+++ android/app/src/main/kotlin/com/example/electronic_fluid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.electronic_fluid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:electronic_fluid/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ),
+  );
+}
+
